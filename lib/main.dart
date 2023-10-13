8e2cecea4e038fa08910e9fd8bb20ff4ca1eec11
ogios <2134692955@qq.com>
2023-10-13 17:33:14 +0800

    done

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,15 @@ import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:ogios_sutils/buffer.dart';
+import 'package:ogios_sutils/in.dart';
+import 'package:ogios_sutils/out.dart';
 
 Future<void> test() async {
   Socket s = await Socket.connect("localhost", 15002);
   SocketBuffer buffer = SocketBuffer();
   s.write("shit");
   s.listen((event) {
+    print(event.length);
     buffer.add(event);
   }, onDone: () {
     buffer.done();
@@ -19,13 +22,13 @@ Future<void> test() async {
   });
   try {
     while (true) {
-      Uint8List bs = await buffer.readN(1);
-      print(bs);
+      Uint8List bs = await buffer.readN(1024);
+      // print("getbs");
     }
   } catch (err) {
     print("ERROR: $err");
   }
-  s.destroy();
+  print("done");
 }
 
 void test1() {
@@ -33,7 +36,37 @@ void test1() {
   print(a.sublist(0, 0));
 }
 
+Future test2_server() async {
+  ServerSocket serverSocket = await ServerSocket.bind("localhost", 15002);
+  await for (var socket in serverSocket) {
+    print("received new conn: ${socket.remoteAddress}");
+    SocketOut so = SocketOut();
+    so.addBytes(Uint8List.fromList("hi from server".codeUnits));
+    await so.writeTo(socket);
+    // socket.close();
+    break;
+  }
+  print("server done.");
+}
+
+Future test2_client() async {
+  Socket s = await Socket.connect("localhost", 15002);
+  SocketIn si = SocketIn(conn: s);
+  int len = await si.next();
+  print("sec length: $len");
+  Uint8List sec = await si.getSec();
+  print("sec: $sec");
+  s.close();
+  print("client done");
+}
+
+void test2() async {
+  test2_server();
+  test2_client();
+}
+
 void main() {
-  test();
+  // test();
   // test1();
+  test2();
 }
---
+++ lib/out.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+class SocketOut {
+  SocketOut();
+  static const int OUT_TYPE_BYTES = 1;
+  static const int OUT_TYPE_READER = 2;
+
+  List<Object> raw = [];
+  List<int> types = [];
+
+  void add(Object raw, int t, Uint8List lenb) {
+    this.raw.add(lenb);
+    this.raw.add(raw);
+    this.types.add(t);
+  }
+
+  Uint8List _main(int last, int index) {
+    Uint8List bytes;
+    if (last >= 255) {
+      int current = last % 255;
+      bytes = this._main((last / 255).toInt(), index + 1);
+      bytes[index] = current;
+    } else {
+      bytes = Uint8List(index + 2);
+      bytes[index] = last;
+      bytes[index + 1] = 255;
+    }
+    return bytes;
+  }
+
+  Uint8List getLength(int length) {
+    return this._main(length, 0);
+  }
+
+  void addBytes(Uint8List raw) {
+    int length = raw.length;
+    Uint8List content_length = this.getLength(length);
+    this.add(raw, OUT_TYPE_BYTES, content_length);
+  }
+
+  void addReader(File raw, int length) {
+    Uint8List content_length = this.getLength(length);
+    this.add(raw, OUT_TYPE_READER, content_length);
+  }
+
+  Future writeTo(Socket writer) async {
+    for (int i=0; i< this.raw.length; i++) {
+      Object input = this.raw[i];
+      if (i%2 == 0) {
+        writer.add(input as Uint8List);
+      } else {
+        int t = this.types[((i-1)/2).toInt()];
+        switch (t) {
+          case OUT_TYPE_BYTES:
+            writer.add(input as Uint8List);
+            writer.flush();
+            break;
+          case OUT_TYPE_READER:
+            Stream reader = input as Stream;
+            bool done = false;
+            reader.listen((event) async {
+              writer.add(event);
+              writer.flush();
+            }, onError: (err){
+              throw err;
+            }, onDone: (){
+              done = true;
+            });
+            while (done) {
+              await Future.delayed(Duration(milliseconds: 10));
+            }
+            break;
+          default:
+            break;
+        }
+      }
+    }
+    // writer.flush();
+  }
+}
